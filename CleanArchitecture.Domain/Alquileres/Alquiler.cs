196a3ad6776cbfb09e1be4d2b620cf2deaedc11d
alexgonzalese <alexgonzalese@gmail.com>
2024-12-22 18:22:08 +0100

    domain services ddd

--- CleanArchitecture.Domain/Alquileres/Alquiler.cs
+++ CleanArchitecture.Domain/Alquileres/Alquiler.cs
@@ -1,4 +1,5 @@
 using CleanArchitecture.Domain.Abstractions;
+using CleanArchitecture.Domain.Alquileres.Events;
 using CleanArchitecture.Domain.Vehiculos;
 
 namespace CleanArchitecture.Domain.Alquileres;
@@ -42,4 +43,30 @@ public sealed class Alquiler : Entity
     public DateTime? FechaNegacion { get; private set; }
     public DateTime? FechaCompletado { get; private set; }
     public DateTime? FechaCancelacion { get; private set; }
+
+    public static Alquiler Reservar(
+        Guid vehiculoId,
+        Guid userId,
+        DateRange? duracion,
+        DateTime? fechaCreacion,
+        PrecioDetalle precioDetalle
+    )
+    {
+        var alquiler = new Alquiler(
+            Guid.NewGuid(),
+            vehiculoId,
+            userId,
+            precioDetalle.PrecioPorPeriodo,
+            precioDetalle.Mantenimeinto,
+            precioDetalle.Accesorios,
+            precioDetalle.PrecioTotal,
+            AlquilerStatus.Reservado,
+            duracion,
+            fechaCreacion
+        );
+
+        alquiler.RaiseDomainEvent(new AlquilerReservadoDomainEvent(alquiler.Id));
+
+        return alquiler;
+    }
 }
---
+++ CleanArchitecture.Domain/Alquileres/PrecioDetalle.cs
@@ -0,0 +1,5 @@
+using CleanArchitecture.Domain.Vehiculos;
+
+namespace CleanArchitecture.Domain.Alquileres;
+
+public record PrecioDetalle(Moneda PrecioPorPeriodo, Moneda Mantenimeinto, Moneda Accesorios, Moneda PrecioTotal);
