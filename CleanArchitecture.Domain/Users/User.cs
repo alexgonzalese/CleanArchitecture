a157f3ac64cb58d8f0dd82a580239fcbd174eff1
alexgonzalese <alexgonzalese@gmail.com>
2024-12-22 16:38:39 +0100

    event notifications

--- CleanArchitecture.Domain/Users/User.cs
+++ CleanArchitecture.Domain/Users/User.cs
@@ -1,10 +1,26 @@
+using System.Runtime.InteropServices.JavaScript;
 using CleanArchitecture.Domain.Abstractions;
+using CleanArchitecture.Domain.Users.Events;
 
 namespace CleanArchitecture.Domain.Users;
 
 public sealed class User : Entity
 {
-    public User(Guid id) : base(id)
+    private User(Guid id, Nombre nombre, Apellido apellido, Email email) : base(id)
     {
+        Nombre = nombre;
+        Apellido = apellido;
+        Email = email;
+    }
+
+    public Nombre? Nombre { get; private set; }
+    public Apellido? Apellido { get; private set; }
+    public Email? Email { get; private set; }
+
+    public static User Create(Nombre nombre, Apellido apellido, Email email)
+    {
+        var user = new User(Guid.NewGuid(), nombre, apellido, email);
+        user.RaiseDomainEvent(new UserCreatedDomainEvent(user.Id));
+        return user;
     }
 }
